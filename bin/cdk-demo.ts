7dd11d0c5a00789cd4d3ba4dc2cbb6e347ca0508
Elisha Lai <elisha.lai@icloud.com>
2022-10-24 16:14:24 -0400

    chore: update dependencies

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
-import "source-map-support/register";
-
 import * as cdk from "aws-cdk-lib";
 
 import { CdkDemoStack } from "../lib/cdk-demo-stack";
 
 const app = new cdk.App();
 const stack = new CdkDemoStack(app, "CdkDemoStack", {
-  env: { account: "064103198336", region: "us-east-1" },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_ACCOUNT,
+  },
 });
 
 cdk.Tags.of(stack).add("App", "Demo");
